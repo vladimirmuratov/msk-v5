e753aa16bf9944f5d039a3a620309e5562d4ab2a
vladimir <vladimirmuratov@mail.ru>
2024-04-10 16:49:15 +0300

    update message

--- src/pages/api/send-mail.js
+++ src/pages/api/send-mail.js
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer'
 
 export default function handler(req, res) {
 
-    const message = {
+    const message1 = {
         from: process.env.ADDRESS_FROM,
         to: [process.env.ADDRESS_TO_1, process.env.ADDRESS_TO_2, process.env.ADDRESS_TO_3, process.env.ADDRESS_TO_4],
         subject: `Поступила заявка на звонок: ${new Date().toLocaleString('ru-RU', {timeZone: 'Europe/Moscow'})}`,
@@ -11,7 +11,8 @@ export default function handler(req, res) {
                 <p>Дата рождения: ${req.body.dateBirth ?? 'Нет'}</p>
                 <p>Телефон: ${req.body.phone}</p>
                 <p>Email: ${req.body.email ?? 'Нет'}</p>
-                <p>Информация: ${req.body.info ?? 'Нет'}</p>`,
+                <p>Информация: ${req.body.info ?? 'Нет'}</p>
+                <p>Сообщение пришло с сайта: mskdoctor.ru</p>`,
     }
 
     let transporter = nodemailer.createTransport({
@@ -23,7 +24,7 @@ export default function handler(req, res) {
     })
 
     if (req.method === 'POST') {
-        transporter.sendMail(message, (err, info) => {
+        transporter.sendMail(message1, (err, info) => {
 
             if (err) {
                 res.status(404).json({
@@ -35,5 +36,22 @@ export default function handler(req, res) {
                 })
             }
         })
+    }
+
+    //-------------------------------------
+
+    if(req.body.email){
+        const message2 = {
+            from: process.env.ADDRESS_FROM,
+            to: [req.body.email],
+            subject: `Вы оставили заявку на звонок на сайте mskdoctor.ru в ${new Date().toLocaleString('ru-RU', {timeZone: 'Europe/Moscow'})}`,
+            html: ` <p>Имя: ${req.body.person}</p>
+                    <p>Дата рождения: ${req.body.dateBirth ?? 'Нет'}</p>
+                    <p>Телефон: ${req.body.phone}</p>
+                    <p>Email: ${req.body.email ?? 'Нет'}</p>
+                    <p>Информация: ${req.body.info ?? 'Нет'}</p>`,
+        }
+
+        transporter.sendMail(message2)
     }
 }
